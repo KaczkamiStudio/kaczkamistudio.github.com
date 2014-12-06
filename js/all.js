f5f8deb3f369af6442757cad5dffcd94380ef79a
Mandaryn <mandaryn@ragnarson.com>
2014-12-06 22:35:40 +0100

    Automated commit at 2014-12-06 21:35:39 UTC by middleman-deploy 0.2.4

--- js/all.js
+++ js/all.js
@@ -12031,6 +12031,15 @@ if (typeof define === 'function' && define.amd) {
     });
 }
 ;
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-35306022-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
 
 
 
