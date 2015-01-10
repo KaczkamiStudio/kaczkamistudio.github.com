2c3650161c3e582aaffa5e680460eb3e57de9f88
Mandaryn <mandaryn@ragnarson.com>
2015-01-10 18:45:30 +0100

    Automated commit at 2015-01-10 17:45:30 UTC by middleman-deploy 0.2.4

--- js/all.js
+++ js/all.js
@@ -12031,6 +12031,36 @@ if (typeof define === 'function' && define.amd) {
     });
 }
 ;
+function KaczkamiAudio(elementId) {
+  var self = this;
+  self.audio = document.getElementById('oj_boli_audio');
+  self.from = 0;
+  self.to = 0;
+  self.mute = true;
+  self.toggle = function() {
+    self.mute = !self.mute;
+  };
+  self.isOn = function() {
+    return self.mute;
+  }
+  self.start = function(span) {
+    self.from = span.from;
+    self.to = span.to;
+    self.audio.currentTime = self.from;
+    self.audio.addEventListener("timeupdate", self.stopListener);
+    self.audio.play();
+  };
+  self.stopListener = function() {
+    if (this.currentTime >= self.to) {
+      this.pause();
+    }
+  }
+  self.stop = function() {
+    self.audio.removeEventListener("timeupdate", self.stopListener);
+    self.audio.pause();
+  }
+}
+;
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-35306022-1']);
 _gaq.push(['_trackPageview']);
